a107495a24da6d67530a94974236d40c4f828f12
Orell Bühler <orell.buehler@students.fhnw.ch>
2025-02-10 13:01:46 +0100

    update lsp config

--- init.lua
+++ init.lua
@@ -677,8 +677,8 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
+        gopls = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -690,8 +690,7 @@ require('lazy').setup({
         --
         tailwindcss = {},
         csharp_ls = {},
-        biome = {},
-
+        eslint = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
